58386ee25fdb44de6b0949a3353c0f870239e2b8
Romina <romina.ghaderi@outlook.com>
2021-12-10 16:33:59 +0100

    new changes create-post.js

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,37 +1,40 @@
-
-let form = document.getElementById("Createpost-form")
-
-window.onload = function() {
-
-form.addEventListener("submit", async function(e) {
+const form = document.getElementById("Createpost-form").addEventListener("submit", async (e) =>  {
     e.preventDefault;
-
-let formdata = new FormData(form);
-formdataObject = {
+/*
+    let formdata = new FormData(form);
+    formdataObject = {
     "title": formdata.get("Title"),
-    "author": formdata.get("Author"),
     "content": formdata.get("Content"),
-}
+    "author": formdata.get("Author"),
+  }
+
+    console.log(JSON.stringify(formdataObject))
+    */
 
 
-    //console.log(JSON.stringify(formdataObject))
-    
+    const Title = document.getElementById("Title-input").value
+    const Author = document.getElementById("Author-input").value
+    const Content = document.getElementById("content-area").value
+
+    const data = {
+        "title": Title, 
+        "author": Author,
+        "content": Content
+    }
+
+    console.log(JSON.stringify(data))
+
 
     try {
-        await fetch('http://localhost:5000/posts'), {
+        await fetch('http://localhost:5000/posts', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+           'Content-Type': 'application/json',
             },
-            body: JSON.stringify(formdataObject),
-        }
-
-       location.replace('index.html')
+            body: JSON.stringify(data),
+        })
 
     } catch(error) {
         console.log(error)
-    
-    
-    }
+    } 
 });
-}
