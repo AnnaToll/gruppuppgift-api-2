7e17e2d06504dde5165b28af84890f032a98d3ca
MrSkleletal <Borgfred@hotmail.se>
2021-12-14 13:40:13 +0100

    started trying to make tags show on update

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,17 +1,34 @@
 let urlParameters = new URLSearchParams(location.search);
     let id = urlParameters.get('id');
 
-let contentTitle = document.getElementById("Title-input")
-let contentAuthor = document.getElementById("Author-input")
-let contentContent = document.getElementById("content-area")
+let contentTitle    = document.getElementById("Title-input")
+let contentAuthor   = document.getElementById("Author-input")
+let contentContent  = document.getElementById("content-area")
+
+let contentTags     = document.getElementById("tagSelect")
+let Tag1            = document.getElementById("tag1")
+let Tag2            = document.getElementById("tag2")
+let Tag3            = document.getElementById("tag3")
+let Tag4            = document.getElementById("tag4")
+
+function isTagThere(theTag) {
+    console.log(theTag)
+    for (let i = 5; i>=0; i--){
+        if (theTag[i] === `Tag${i+1}`){
+            console.log(i+1)
+        }
+    }
+}
 
 try {
      fetch(`http://localhost:5000/posts/${id}`)
      .then(response => response.json())
      .then(data => {
-        contentTitle.value = (data.title)
-        contentAuthor.value = (data.author)
-        contentContent.innerHTML = (data.content)
+        contentTitle.value          = (data.title)
+        contentAuthor.value         = (data.author)
+        contentContent.innerHTML    = (data.content)
+        isTagThere(data.tags)
+        
     });
     
     
@@ -24,13 +41,13 @@ const form = document.getElementById("Createpost-form").addEventListener("submit
 
 
     const selected = []
-    for (var option of document.getElementById('tagSelect').options) {
+    for (var option of contentTags.options) {
         if (option.selected == true) {
             selected.push(option.value);
         }
     }
 
-    
+
     const Title = document.getElementById("Title-input").value
     const Author = document.getElementById("Author-input").value
     const Content = document.getElementById("content-area").value
@@ -38,7 +55,8 @@ const form = document.getElementById("Createpost-form").addEventListener("submit
     const data = {
         "title": Title, 
         "author": Author,
-        "content": Content
+        "content": Content,
+        "tags": selected
     }
 
     console.log(JSON.stringify(data))
