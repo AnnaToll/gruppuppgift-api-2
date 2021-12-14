55f2d9bca144f8bf3ed225e86bbf0fc24a9d8d21
Anna Toll <anna.toll@protonmail.com>
2021-12-14 16:35:42 +0100

    init post.html

---
+++ js/post.js
@@ -0,0 +1,10 @@
+/* let main = document.querySelector('main');
+
+let urlParameters = new URLSearchParams(location.search);
+let id = urlParameters.get('id');
+
+async function addContentToMain() {
+    let response = await fetch(`http://localhost:5000/posts/${id}`);
+    let post = await response.json();
+
+} */
