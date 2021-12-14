b940547386ca36d36196a1639b517ec107c67caa
MrSkleletal <Borgfred@hotmail.se>
2021-12-14 12:18:52 +0100

    Tags in update

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -23,6 +23,14 @@ const form = document.getElementById("Createpost-form").addEventListener("submit
     e.preventDefault();
 
 
+    const selected = []
+    for (var option of document.getElementById('tagSelect').options) {
+        if (option.selected == true) {
+            selected.push(option.value);
+        }
+    }
+
+    
     const Title = document.getElementById("Title-input").value
     const Author = document.getElementById("Author-input").value
     const Content = document.getElementById("content-area").value
